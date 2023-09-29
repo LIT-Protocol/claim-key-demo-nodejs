bd81259956d40b1551a2d1378dd5558e9433622c
Josh Long <josh@litprotocol.com>
2023-09-29 17:17:10 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lit PKP claim demo for NodeJS enviorment
+
+Demo for showcasing claimable key flow, `Auth Method` used in this demo is `stytch` showcasing
+- How to derive a key from a stytch auth method type
+- How to claim a key from a stytch auth method type
+
+## Usage
+Running the demo for key claiming 
+
+```
+yarn start --claim
+```
+
+Running the demo for querying keys after claiming
+```
+yarn start --lookup
+```
+
+*Note* All code in this demo is runnable in a web context.
---
+++ index.ts
@@ -0,0 +1,97 @@
+import {
+  LitAuthClient,
+  StytchOtpProvider,
+} from "@lit-protocol/lit-auth-client/src/index.js";
+import prompts from "prompts";
+import * as stytch from "stytch";
+import { LitNodeClientNodeJs } from "@lit-protocol/lit-node-client-nodejs";
+import { ProviderType } from "@lit-protocol/constants";
+
+/**
+ * Should be defined in your local enviorment before running
+ * see here: https://stytch.com/docs for setting up your stytch project
+ */
+const STYTCH_PROJECT_ID: string | undefined = process.env.STYTCH_PROJECT_ID;
+const STYTCH_SECRET: string | undefined = process.env.STYTCH_SECRET;
+const LIT_RELAY_API_KEY: string | undefined = process.env.LIT_RELAY_API_KEY;
+
+if (!STYTCH_PROJECT_ID || !STYTCH_SECRET) {
+  throw Error("Could not find stytch project secret or id in enviorment");
+}
+
+if (process.argv.length >= 3) {
+  throw Error("Please provide either --lookup or --claim flag");
+}
+
+const client = new stytch.Client({
+  project_id: STYTCH_PROJECT_ID,
+  secret: STYTCH_SECRET,
+});
+
+const emailResponse = await prompts({
+  type: "text",
+  name: "email",
+  message: "Enter your email:",
+});
+
+const stytchResponse = await client.otps.email.loginOrCreate({
+  email: emailResponse.email,
+});
+
+const otpResponse = await prompts({
+  type: "text",
+  name: "code",
+  message: "Enter the code sent to your email:",
+});
+
+const authResponse = await client.otps.authenticate({
+  method_id: stytchResponse.email_id,
+  code: otpResponse.code,
+  session_duration_minutes: 60 * 24 * 7,
+});
+
+let sessionResp = await client.sessions.get({
+  user_id: authResponse.user_id,
+});
+
+const sessionStatus = await client.sessions.authenticate({
+  session_token: authResponse.session_token,
+});
+
+const litNodeClient = new LitNodeClientNodeJs({
+  litNetwork: "cayenne",
+  debug: false,
+});
+
+await litNodeClient.connect();
+
+const authClient = new LitAuthClient({
+  litRelayConfig: {
+    relayApiKey: LIT_RELAY_API_KEY,
+  },
+  litNodeClient,
+});
+
+const session = authClient.initProvider<StytchOtpProvider>(
+  ProviderType.StytchOtp,
+  {
+    userId: sessionStatus.session.user_id,
+    appId: STYTCH_PROJECT_ID,
+  }
+);
+
+const authMethod = await session.authenticate({
+  accessToken: sessionStatus.session_jwt,
+});
+const publicKey = await session.computPublicKeyFromAuthMethod(authMethod);
+console.log("local public key computed: ", publicKey);
+
+if (process.argv.length >= 3 && process.argv[2] === "--claim") {
+  let claimResp = await session.claimKeyId({
+    authMethod,
+  });
+  console.log("claim response public key: ", claimResp.pubkey);
+} else if (process.argv.length >= 3 && process.argv[2] === "--lookup") {
+  const pkpInfo = await session.fetchPKPsThroughRelayer(authMethod);
+  console.log(pkpInfo);
+}
